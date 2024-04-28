399f425bdde77d1efc6b9927cf9629e781ed953a
SantiagoBrie <155693553+SantiagoBrie@users.noreply.github.com>
2024-04-28 16:41:11 -0300

    Se arregla el arraste de las tareas

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -37,19 +37,24 @@ export default function TaskCard({ task, canEdit }: TaskCardProps) {
         transform: `translate3d(${transform.x}px,${transform.y}px,0)`,
         padding: "1.25rem",
         backgroundColor: '#FFF',
-        width: '600px',
+        width: '280px',
         display: 'flex',
         borderWidth: '1px',
         borderColor: 'rgb(203 213 225 / var(--tw-border-opacity))'
     } : undefined
 
     return (
-        <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
+        <li
+            {...listeners}
+            {...attributes}
+            ref={setNodeRef}
+            style={style}
+            className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
             <div
-                {...listeners}
-                {...attributes}
-                ref={setNodeRef}
-                style={style}
+                // {...listeners}
+                // {...attributes}
+                // ref={setNodeRef}
+                // style={style}
                 className="min-w-0 flex flex-col gap-y-4">
                 <p
                     className="text-xl font-bold text-slate-600 text-left"
--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,4 +1,4 @@
-import { DndContext, DragEndEvent } from "@dnd-kit/core"
+import { DndContext, DragEndEvent, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core"
 import { Project, Task, TaskProject, TaskStatus } from "@/types/index"
 import TaskCard from "./TaskCard"
 import { statusTranslations } from "@/locales/es"
@@ -48,6 +48,21 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
         }
     })
 
+    const mouseSensor = useSensor(MouseSensor, {
+        activationConstraint: {
+            distance: 10,
+        },
+    });
+    
+    const touchSensor = useSensor(TouchSensor, {
+        activationConstraint: {
+            delay: 250,
+            tolerance: 5,
+        },
+    });
+    
+    const sensors = useSensors(mouseSensor, touchSensor);   
+
 
     const groupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -81,31 +96,33 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
     }
 
     return (
-        <>
-            <h2 className="text-5xl font-black my-10">Tareas</h2>
+        <DndContext sensors={sensors} >
+            <>
+                <h2 className="text-5xl font-black my-10">Tareas</h2>
 
-            <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
-                <DndContext onDragEnd={handleDragEnd}>
-                    {Object.entries(groupedTasks).map(([status, tasks]) => (
-                        <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
+                    <DndContext onDragEnd={handleDragEnd}>
+                        {Object.entries(groupedTasks).map(([status, tasks]) => (
+                            <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
 
-                            <h3
-                                className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
-                            >{statusTranslations[status]}</h3>
+                                <h3
+                                    className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+                                >{statusTranslations[status]}</h3>
 
-                            <DropTask status={status} />
+                                <DropTask status={status} />
 
-                            <ul className='mt-5 space-y-5'>
-                                {tasks.length === 0 ? (
-                                    <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
-                                ) : (
-                                    tasks.map(task => <TaskCard key={task._id} task={task} canEdit={canEdit} />)
-                                )}
-                            </ul>
-                        </div>
-                    ))}
-                </DndContext>
-            </div>
-        </>
+                                <ul className='mt-5 space-y-5'>
+                                    {tasks.length === 0 ? (
+                                        <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
+                                    ) : (
+                                        tasks.map(task => <TaskCard key={task._id} task={task} canEdit={canEdit} />)
+                                    )}
+                                </ul>
+                            </div>
+                        ))}
+                    </DndContext>
+                </div>
+            </>
+        </DndContext>
     )
 }
